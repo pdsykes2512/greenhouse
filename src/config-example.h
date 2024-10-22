75f78a97152eb6ae6b76737b42667a409d8da449
Paul Sykes <paul@pdsykes.co.uk>
2024-10-22 06:05:05 +0000

    Remove config.h and sensitive details Create example config file

--- src/config-example.h
+++ src/config-example.h
@@ -1,9 +1,9 @@
 #define SSID "Fallowfield"
-#define PSK "mock.united.marathons"
+#define PSK "Password goes here"
 #define MySQL
-#define mysql_url "http://192.168.11.101/adddata.php"
+#define mysql_url "http://{IP of DB server}/adddata.php"
 #define SYSTEM_HOSTNAME "Greenhouse"
-#define FLASH_PWD "p0UmrR7k6xrp0wzfGNEP"
+#define FLASH_PWD "Password goes here"
 #define TELNET_DEBUG
 #define on      1
 #define off     0
@@ -17,7 +17,7 @@
 #define TEMP1_SENSOR        0
 #define SOIL1_PIN           A0
 #define VALVE1_RELAY        D5
-#define PLANT1_SOIL_DRY     45
+#define PLANT1_SOIL_DRY     40
 #define PLANT1_SOIL_WET     95
 #define PLANT1_WATER_ON     10
 #define PLANT1_WATER_OFF    (6 * 60 * 60)
@@ -27,7 +27,7 @@
 #define TEMP2_SENSOR        1
 #define SOIL2_PIN           A1
 #define VALVE2_RELAY        D4
-#define PLANT2_SOIL_DRY     45
+#define PLANT2_SOIL_DRY     40
 #define PLANT2_SOIL_WET     95
 #define PLANT2_WATER_ON     20
 #define PLANT2_WATER_OFF    (6 * 60 * 60)
@@ -37,7 +37,7 @@
 #define TEMP3_SENSOR        2
 #define SOIL3_PIN           A2
 #define VALVE3_RELAY        D3
-#define PLANT3_SOIL_DRY     45
+#define PLANT3_SOIL_DRY     40
 #define PLANT3_SOIL_WET     95
 #define PLANT3_WATER_ON     10
 #define PLANT3_WATER_OFF    (6 * 60 * 60)
@@ -47,7 +47,7 @@
 #define TEMP4_SENSOR        3
 #define SOIL4_PIN           A3
 #define VALVE4_RELAY        D2
-#define PLANT4_SOIL_DRY     45
+#define PLANT4_SOIL_DRY     40
 #define PLANT4_SOIL_WET     95
 #define PLANT4_WATER_ON     20
 #define PLANT4_WATER_OFF    (6 * 60 * 60)
